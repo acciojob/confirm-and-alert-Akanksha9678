863c831fbdae7509d3eda24f6aa5303acb95f04c
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-03-01 21:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+//your JS code here. If require.
+let response=confirm("Do you want to proceed?")
+if(response==true){
+	alert("You clicked OK. Proceeding...")
+}
+else{
+	alert("You clicked Cancel. Exiting...")
+}
